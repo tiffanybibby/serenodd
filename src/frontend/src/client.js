9cf81ecc169ed9217079de1d7110ff3c9baa38f1
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2022-03-02 15:13:04 -0500

    added client file

---
+++ src/frontend/src/client.js
@@ -0,0 +1,14 @@
+import fetch from "unfetch";
+
+const checkStatus = response => {
+    if (response.ok) {
+        return response;
+    }
+    // convert non-2xx HTTP responses into errors:
+    const error = new Error(response.statusText);
+    error.response = response;
+    return Promise.reject(error);
+}
+
+export const getAllMedias = () =>
+    fetch("api/v1/media").then(checkStatus);
