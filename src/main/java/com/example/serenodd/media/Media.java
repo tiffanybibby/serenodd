0e1cac96dc4f8f60e29513b107437e72a76f3bcd
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2022-03-02 14:06:31 -0500

    updated controller,properties, gitignore, added data.sql file

--- src/main/java/com/example/serenodd/media/Media.java
+++ src/main/java/com/example/serenodd/media/Media.java
@@ -27,6 +27,7 @@ public class Media {
 
     private Long id;
     private String name;
+    @Column(length = 500)
     private String description;
     private String source;
     private String audio;
