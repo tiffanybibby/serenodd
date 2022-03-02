fa4b0d521d1fddca5adb08410a17128de5a06511
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2022-03-01 20:17:24 -0500

    initialized spring boot project using spring initializer

---
+++ serenodd/src/main/java/com/example/serenodd/SerenoddApplication.java
@@ -0,0 +1,13 @@
+package com.example.serenodd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerenoddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerenoddApplication.class, args);
+	}
+
+}
---
+++ serenodd/src/test/java/com/example/serenodd/SerenoddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serenodd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerenoddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
