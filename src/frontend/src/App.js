f771e4a08a97da3bda80e15c51755039a898af51
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2022-03-02 17:33:40 -0500

    installed materialUI and implemented boilerplate layout from antd

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,14 +1,17 @@
-import {getAllMedias} from "./client";
 import {useState, useEffect} from "react";
-import {Empty, Spin} from "antd";
-import { LoadingOutlined } from '@ant-design/icons';
+import {getAllMedias} from "./client";
+import {Empty, Spin, Layout, Menu, Breadcrumb, Table} from "antd";
+import { LoadingOutlined, DesktopOutlined, UserOutlined, FileOutlined, PieChartOutlined, TeamOutlined } from '@ant-design/icons';
 import './App.css';
 
+const { Header, Content, Footer, Sider } = Layout;
+const {SubMenu} = Menu;
 const antIcon = <LoadingOutlined style={{fontSize: 24}} spin/>;
 
 function App() {
     const [medias, setMedias] = useState([]);
     const [fetching, setFetching] = useState(true)
+    const [collapsed, setCollapsed] = useState(false);
 
     const fetchMedias = () =>
         getAllMedias()
@@ -42,10 +45,50 @@ function App() {
     }
 
     return (
-        <div>
-        {renderMedias()}
-        </div>
+        // <div>
+        // {renderMedias()}
+        // </div>
+        <Layout style={{minHeight: '100vh'}}>
+            <Sider collapsible collapsed={collapsed}
+                   onCollapse={setCollapsed}>
+                <div className="logo"/>
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                    <Menu.Item key="1" icon={<PieChartOutlined/>}>
+                        Option 1
+                    </Menu.Item>
+                    <Menu.Item key="2" icon={<DesktopOutlined/>}>
+                        Option 2
+                    </Menu.Item>
+                    <SubMenu key="sub1" icon={<UserOutlined/>} title="User">
+                        <Menu.Item key="3">Tom</Menu.Item>
+                        <Menu.Item key="4">Bill</Menu.Item>
+                        <Menu.Item key="5">Alex</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">
+                        <Menu.Item key="6">Team 1</Menu.Item>
+                        <Menu.Item key="8">Team 2</Menu.Item>
+                    </SubMenu>
+                    <Menu.Item key="9" icon={<FileOutlined/>}>
+                        Files
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout className="site-layout">
+                <Header className="site-layout-background" style={{padding: 0}}/>
+                <Content style={{margin: '0 16px'}}>
+                    <Breadcrumb style={{margin: '16px 0'}}>
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                        {renderMedias()}
+                    </div>
+                </Content>
+                <Footer style={{textAlign: 'center'}}>By Tiffany D ©2022</Footer>
+            </Layout>
+        </Layout>
     )
 }
 
 export default App;
+
